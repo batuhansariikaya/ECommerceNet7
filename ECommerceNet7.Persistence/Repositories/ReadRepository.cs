4b25cd00f649fe5427e33f21109a90526338df30
batuhansariikaya <batuhansarikaya008@gmail.com>
2023-07-21 20:29:45 +0300

    repositorylerin içi dolduruldu

---
+++ ECommerceNet7.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceNet7.Application.Repositories;
+using ECommerceNet7.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceNet7.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECommerceNet7.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceNet7.Application.Repositories;
+using ECommerceNet7.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceNet7.Persistence.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public Task<bool> AddAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
