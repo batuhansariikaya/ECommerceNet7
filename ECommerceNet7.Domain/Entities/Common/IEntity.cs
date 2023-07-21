6d91587b6295affdcb6a2222e7b0708ecc99b858
batuhansariikaya <batuhansarikaya008@gmail.com>
2023-07-21 20:21:31 +0300

    Add project files.

---
+++ ECommerceNet7.Domain/Entities/Common/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceNet7.Domain.Entities.Common
+{
+    public interface IEntity
+    {
+    }
+}
